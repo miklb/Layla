8644a427a5242cf93d9437361d2516962e464380
Michael Bishop <miklb@miklb.com>
2011-10-29 16:13:58 -0400

    initial child theme commit

---
+++ theme.php
@@ -0,0 +1,15 @@
+<?php
+
+class Layla extends Mzingi2
+{
+
+	public function action_add_template_vars($theme) {
+
+		parent::action_add_template_vars($theme);
+		Stack::add('template_stylesheet', array(Site::get_url( 'theme', '/css/style.css' )), 'theme');
+
+	}
+
+}
+
+?>
