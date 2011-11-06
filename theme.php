3b5346a05362aa67842958a1f72c0080f5aea0ad
Michael Bishop <miklb@miklb.com>
2011-11-06 01:19:08 -0400

    It's actually not required to add the comment form function in the child theme

--- theme.php
+++ theme.php
@@ -9,10 +9,6 @@ class Layla extends Mzingi2
 		Stack::add('template_stylesheet', array(Site::get_url( 'theme', '/css/style.css' )), 'theme');
 
 	}
-	
-	public function action_form_comment( $form ) {
-		parent::action_form_comment($form);
-	}
 
 }
 
